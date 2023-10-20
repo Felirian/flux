0c7f1d2c48824495f8579c0c1e4c8ab4f946fb8c
Felirian <hhghv49@gmail.com>
2023-10-20 19:55:24 +0300

    fix: next5

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
-import '../style/index.css'
+import '../style/index.sass'
 const _App = ({Component, pageProps}) => {
 
   useEffect(() => {
