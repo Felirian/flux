e0d05c23295f01e373f343d7b663d24b86161652
Felirian <hhghv49@gmail.com>
2023-10-27 19:01:28 +0300

    add: ***5

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,18 +4,18 @@ import Footer from "@/components/Footer/Footer";
 import '../style/styles.scss'
 const _App = ({Component, pageProps}) => {
 
-  // useEffect(() => {
-  //   let cards = document.querySelectorAll(".Button");
-  //   document.addEventListener('mousemove', e => {
-  //     for (let i = 0; i < cards.length; i++) {
-  //       const rect = cards[i].getBoundingClientRect(),
-  //         x = e.clientX - rect.left,
-  //         y = e.clientY - rect.top;
-  //       cards[i].style.setProperty("--mouse-x", `${x}px`);
-  //       cards[i].style.setProperty("--mouse-y", `${y}px`);
-  //     }
-  //   });
-  // }, []);
+  useEffect(() => {
+    let cards = document.querySelectorAll(".Button");
+    document.addEventListener('mousemove', e => {
+      for (let i = 0; i < cards.length; i++) {
+        const rect = cards[i].getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+        cards[i].style.setProperty("--mouse-x", `${x}px`);
+        cards[i].style.setProperty("--mouse-y", `${y}px`);
+      }
+    });
+  }, []);
   return (
     <>
       <Header/>
