f18431cebcb1992c223fc9eaba971ea891a7ae21
Felirian <141245468+Felirian@users.noreply.github.com>
2023-11-12 19:00:29 +0300

    create: context

---
+++ src/components/Context.js
@@ -0,0 +1,4 @@
+import { createContext } from 'react';
+export const authContext = createContext();
+export const adminContext = createContext();
+export const themeContext = createContext();
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,13 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import {authContext, adminContext, themeContext} from "@/components/Context";
 import '../style/styles.scss'
 const _App = ({Component, pageProps}) => {
+  const [auth, setAuth] = useState(false)
+  const [admin, setAdmin] = useState(false)
+  const [theme, setTheme] = useState('dark')
+
   useEffect(() => {
     let cards = document.querySelectorAll(".Button");
     document.addEventListener('mousemove', e => {
@@ -19,8 +24,12 @@ const _App = ({Component, pageProps}) => {
     <>
       <Header/>
       <main>
-        <Component {...pageProps} />
-        <Footer/>
+        <authContext.Provider value={[auth, setAuth]}>
+          <adminContext.Provider value={[auth, setAuth]}>
+            <Component {...pageProps} />
+            <Footer/>
+          </adminContext.Provider>
+        </authContext.Provider>
       </main>
 
     </>
