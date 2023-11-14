a409602729c98dd9460a1dd1a028a0c03125967a
Felirian <141245468+Felirian@users.noreply.github.com>
2023-11-14 13:42:50 +0300

    fix: database rest

--- src/supabase/services.js
+++ src/supabase/services.js
@@ -1,7 +1,8 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, } from '@supabase/supabase-js'
 import {useEffect, useState} from "react";
 
-const supabase_key = process.env.NEXT_PUBLIC_DB_API_KEY
+
+const supabase_key = process.env.NEXT_PUBLIC_DB_SERVICE_KEY
 const supabase_url = process.env.NEXT_PUBLIC_DB_URL
 const supabase = createClient(supabase_url, supabase_key);
 
