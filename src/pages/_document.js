703bd0ba545384df3af1bfc0e1f79fe897833dcf
Felirian <141245468+Felirian@users.noreply.github.com>
2023-11-14 11:28:50 +0300

    add: favicon

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -24,37 +24,17 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
           />
-          <link
-            rel="apple-touch-icon"
-            sizes="72x72"
-            href="/favicon/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicon/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicon/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicon/site.webmanifest"/>
-          <link
-            rel="mask-icon"
-            href="/favicon/safari-pinned-tab.svg"
-            color="#5bbad5"
-          />
-          <meta name="msapplication-TileColor" content="#da532c"/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/site.webmanifest"/>
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0d0d0d"/>
+          <meta name="msapplication-TileColor" content="#ff0000"/>
           <meta name="theme-color" content="#ffffff"/>
         </Head>
         <body>
         <Main/>
         <NextScript/>
-
-
 
 
         </body>
